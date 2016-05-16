37f0bcd5e735fec14335edffa9528495a39033b7
John Whittenburg <jawhittenburg@gmail.com>
2016-05-16 12:40:55 -0500

    Add end user to responses automatically

--- server/models/response.js
+++ server/models/response.js
@@ -1,3 +1,20 @@
 module.exports = function(Response) {
+  Response.beforeRemote(`create`, (context, user, callback) => {
+    // Get the request from the server context
+    const request = context.req;
 
+    // Get current user id
+    const userId = request.accessToken.userId;
+
+    request.body.data.relationships = request.body.data.relationships || {};
+    request.body.data.relationships['end-user'] = request.body.data.relationships['end-user'] || {};
+
+    // Set relationship data for user
+    request.body.data.relationships['end-user'].data = {
+      id: userId,
+    };
+
+    // Continue creating the recipe
+    callback();
+  });
 };
